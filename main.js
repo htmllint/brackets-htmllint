ab8ff19daa94cf04a0420a0176b30fc8b039e9da
messman <andrewmess93@gmail.com>
2014-12-05 21:29:14 -0500

    Added async tasks

--- main.js
+++ main.js
@@ -1,36 +1,148 @@
 define(function (require, exports, module) {
     "use strict";
     var namespace = 'brackets-htmllint';
+    var _configFileName = ".htmllintrc";
 
     var CodeInspection = brackets.getModule("language/CodeInspection"),
         ExtensionUtils = brackets.getModule("utils/ExtensionUtils"),
-        NodeDomain = brackets.getModule("utils/NodeDomain");
+        NodeDomain = brackets.getModule("utils/NodeDomain"),
+        FileSystem = brackets.getModule("filesystem/FileSystem"),
+        FileUtils = brackets.getModule("file/FileUtils"),
+        ProjectManager = brackets.getModule("project/ProjectManager");
 
     var htmllintDomain = new NodeDomain("htmllint", ExtensionUtils.getModulePath(module, "node/htmllintDomain"));
 
-    function handleLinter(html, fullPath, options) {
-        var defer = new $.Deferred();
-        htmllintDomain.exec("lint", html)
-            .done(function (errors) {
-                var result = {
-                    errors: [],
-                };
-                errors.forEach(function (error) {
-                    console.log("old error: ", error);
-                    var returnedError = {
-                        pos: {line: error.line, col: error.column},
-                        message: error.name + ": " + error.msg,
-                        type: CodeInspection.Type.ERROR
-                    };
-                    console.log("new error: ", returnedError);
-                    result.errors.push(returnedError);
-                });
-                defer.resolve(result);
 
-            }).fail(function (err) {
-                console.error("Failed: ", err);
-            });
+
+    function handleLinter(html, fullPath) {
+        var defer = new $.Deferred();
+        loadConfigs(fullPath).done(function (options) {
+            console.log("options: ", options);
+
+            htmllintDomain.exec("lint", html)
+                .done(function (errors) {
+                    // Success!
+                    var result = {
+                        errors: [],
+                    };
+                    errors.forEach(function (error) {
+                        var newCode = "(" + error.code + ")";
+                        var returnedError = {
+                            pos: {
+                                line: error.line - 1, // I don't know why.
+                                col: error.column
+                            },
+                            message: newCode,
+                            type: CodeInspection.Type.ERROR
+                        };
+
+                        // get the message!
+                        htmllintDomain.exec("getMessage", error.code, error.data)
+                            .done(function (message) {
+                                returnedError.message = message + " " + newCode;
+                            });
+                        result.errors.push(returnedError);
+                    });
+                    defer.resolve(result);
+                }).fail(function (err) {
+                    console.error("Failed: ", err);
+                    defer.reject(err);
+                });
+        })
         return defer.promise();
+    }
+
+    /*
+     * Takes the full path to the file, provided via Brackets.
+     * Does a lookup starting from the project root down to the directory of the current file.
+     * When it finds a valid .htmllintrc file, it will parse the JSON and add it to the options,
+     * overwriting previous options.
+     * Returns the final options set.
+     */
+    function loadConfigs(fullPath) {
+        var projectRootEntry = ProjectManager.getProjectRoot(),
+            result = new $.Deferred(),
+            relPath,
+            rootPath,
+            finalConfigs = [];
+
+        // if nothing is open, return [] as the option.
+        if (!projectRootEntry) {
+            return result.resolve([]).promise();
+        }
+
+        // get the full absolute path to this directory.
+        rootPath = projectRootEntry.fullPath;
+        console.log("rootPath", rootPath);
+        // get the path relative to the root of the project.
+        relPath = FileUtils.getRelativeFilename(rootPath, fullPath);
+        console.log("relPath before", relPath);
+
+        // for files outside the root, use default config
+        if (!relPath) {
+            return result.resolve([]).promise();
+        }
+
+        // get the parent directory of our relative path (to remove the *.html part)
+        relPath = FileUtils.getDirectoryPath(relPath);
+        console.log("relPath after", relPath);
+
+        function addToArray(configs) {
+            console.log(configs);
+            finalConfigs = finalConfigs.concat(configs);
+        }
+
+        getConfig(rootPath + relPath).done(addToArray);
+
+        while (relPath.length > 0) {
+            // take off the '/' end of the path to get the new parent directory
+            relPath = FileUtils.getDirectoryPath(relPath.substr(0, relPath.length - 1));
+            console.log("stripping to: ", relPath);
+            getConfig(rootPath + relPath).done(addToArray);
+        }
+
+        //        _lookupAndLoad(rootPath, relPath, _readConfig)
+        //            .done(function (cfg) {
+        //                result.resolve(cfg);
+        //            });
+        console.log("returning: ", finalConfigs);
+        return result.resolve(finalConfigs).promise();
+    }
+
+    /* 
+     * Takes a directory.
+     * Uses the directory to look for the ".htmllintrc" file
+     * and JSON parses it.
+     * Returns the object.
+     */
+    function getConfig(directory) {
+        var result = new $.Deferred(),
+            filePath = directory + _configFileName,
+            file;
+        console.log("searching for: ", filePath);
+
+        // get the file object for the filename.
+        file = FileSystem.getFileForPath(filePath);
+        file.read(function (err, content) {
+            if (!err) {
+                var config = [];
+                try {
+                    config = JSON.parse(content);
+                } catch (e) {
+                    console.error("brackets-htmllint: error parsing " + file.fullPath + ". Details: " + e);
+                    result.reject(e);
+                    return;
+                }
+                console.log("Found file at " + filePath + "!!!", config);
+                result.resolve(config);
+            } else {
+                // file does not exist.
+                console.log("brackets-htmllint: error finding file " + filePath + ". Details: " + err);
+                result.resolve(["not found at " + filePath]);
+            }
+        });
+        //result.resolve(["hello"]);
+        return result.promise();
     }
 
     CodeInspection.register("html", {
--- node/htmllintDomain.js
+++ node/htmllintDomain.js
@@ -1,44 +1,70 @@
 (function () {
     "use strict";
-    
+
     var htmllint = require("htmllint");
-    /**
-     * @private
-     * Handler function for htmllint
-     * @param {string} html The html to check
-     * @param {array} opts The options to use
-     * @return {array} The list of errors.
-     */
+
     function lint(html, opts) {
         return htmllint(html, opts);
     }
-    
+
+    function getMessage(code, data) {
+        var message = htmllint.messages.renderMsg(code, data);
+        //console.log(code + ": " + message);
+        return message;
+    }
+
     /**
      * Initializes the test domain with several test commands.
      * @param {DomainManager} domainManager The DomainManager for the server
      */
     function init(domainManager) {
         if (!domainManager.hasDomain("htmllint")) {
-            domainManager.registerDomain("htmllint", {major: 0, minor: 1});
+            domainManager.registerDomain("htmllint", {
+                major: 0,
+                minor: 1
+            });
         }
         domainManager.registerCommand(
-            "htmllint",       // domain name
-            "lint",    // command name
-            lint,   // command handler function
-            true,          // this command is synchronous in Node
-            "Returns the errors in linted html",
-            [{name: "html", // parameters
+            "htmllint", // domain name
+            "lint", // command name
+            lint, // command handler function
+            true, // this command is asynchronous in Node
+            "Returns the errors in linted html", [{
+                    name: "html", // parameters
+                    type: "string",
+                    description: "the string of html to check"
+                },
+                {
+                    name: "opts", // parameters
+                    type: "array",
+                    description: "the options to pass in"
+                }], [{
+                name: "errors", // return values
+                type: "array",
+                description: "the errors from the linted html"
+            }]
+        );
+        domainManager.registerCommand(
+            "htmllint", // domain name
+            "getMessage", // command name
+            getMessage, // command handler function
+            false, // this command is synchronous in Node
+            "Gets the proper error message for a code", [{
+                name: "code", // parameters
                 type: "string",
-                description: "the string of html to check"},
-             {name: "opts", // parameters
-                type: "array",
-                description: "the options to pass in"}],
-            [{name: "errors", // return values
-                type: "array",
-                description: "the errors from the linted html"}]
+                description: "the code to check"
+                }, {
+                name: "data", // parameters
+                type: "object",
+                description: "the object to pass in for data binding"
+                }], [{
+                name: "message", // return values
+                type: "string",
+                description: "the correct message"
+            }]
         );
     }
-    
+
     exports.init = init;
-    
+
 }());
